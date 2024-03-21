c2b3a5dafa35a06fa3fd74d59d8c0e3937efaba2
Avita-jpg <juanitaortizg@gmail.com>
2024-03-21 19:18:17 +0100

    AfficherMarche

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,24 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerEtatMarche();
+		if (infosMarche.length == 0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			System.out.println(nomAcheteur + ", vous trouverez au marché:");
+			int i = 0;
+			while (i < infosMarche.length-1) {
+				StringBuilder chaine = new StringBuilder();
+				chaine.append("- ");
+				chaine.append(infosMarche[i]);
+				i++;
+				chaine.append(" qui vend ");
+				chaine.append(infosMarche[i]);
+				i++;
+				chaine.append(" ");
+				chaine.append(infosMarche[i]);
+				System.out.println(chaine.toString());
+			}
+		}
 	}
 }
